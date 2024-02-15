6d4524a2ef945336c3df4cd72262adbe5433c3e8
ekohadipermana <ekohadipermana@gmail.com>
2024-02-15 17:48:00 +0700

    creating elements

--- script.js
+++ script.js
@@ -1,1 +1,13 @@
 'use strict';
+// selecting elements
+const score0Element = document.querySelector("#score--0");
+const score1Element = document.querySelector("#score--1");
+const btnNewElement = document.querySelector(".btn--new");
+const btnHoldElement = document.querySelector(".btn--hold");
+const btnRollElement = document.querySelector(".btn--roll");
+
+const diceElement = document.querySelector('.dice');
+
+diceElement.classList.add('hidden');
+score0Element.textContent = 0;
+score1Element.textContent = 0;
