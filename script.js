c126b6e39a62fb87d6224dfe383d6fb783f5e2c1
ekohadipermana <ekohadipermana@gmail.com>
2024-02-15 18:03:49 +0700

    creating random funcionality

--- script.js
+++ script.js
@@ -5,9 +5,17 @@ const score1Element = document.querySelector("#score--1");
 const btnNewElement = document.querySelector(".btn--new");
 const btnHoldElement = document.querySelector(".btn--hold");
 const btnRollElement = document.querySelector(".btn--roll");
-
 const diceElement = document.querySelector('.dice');
 
+// first condition
 diceElement.classList.add('hidden');
 score0Element.textContent = 0;
 score1Element.textContent = 0;
+
+// Rolling Dice
+const createRandomNum = function () {
+    // Math.random() * 6 + 1;
+    console.log(Math.floor(Math.random() * 6 + 1));
+};
+
+btnRollElement.addEventListener('click', createRandomNum);
