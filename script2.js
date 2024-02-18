43631b718857eeeb4ed4a4d2fdf58bc8dd9e28da
ekohadipermana <ekohadipermana@gmail.com>
2024-02-18 08:17:26 +0700

    test again the button elements for DOM

--- script2.js
+++ script2.js
@@ -3,12 +3,23 @@ const score0El = document.querySelector('#score--0');
 const score1El = document.querySelector('#score--1');
 const current0El = document.querySelector('#current--0');
 const current1El = document.querySelector('#current--1');
-const buttonNew = document.querySelector('btn--new');
-const buttonRoll = document.querySelector('btn--roll');
-const buttonHold = document.querySelector('btn--hold');
+const buttonNew = document.querySelector('.btn--new');
+const buttonRoll = document.querySelector('.btn--roll');
+const buttonHold = document.querySelector('.btn--hold');
 
 // test
 score0El.textContent = 99;
 score1El.textContent = 99;
 current0El.textContent = 99;
 current1El.textContent = 99;
+
+// button test
+buttonNew.addEventListener('click', function() {
+    console.log('new clicked')
+});
+buttonHold.addEventListener('click', function() {
+    console.log('Hold clicked')
+});
+buttonRoll.addEventListener('click', function() {
+    console.log('Roll clicked')
+});
