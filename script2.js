1162813acfd78ba14e9b286874451ac3023f9a41
ekohadipermana <ekohadipermana@gmail.com>
2024-02-18 08:05:40 +0700

    creating elements for DOM

--- script2.js
+++ script2.js
@@ -1,1 +1,5 @@
 'use strict';
+const score0El = document.querySelector('score--0');
+const score1El = document.querySelector('score--1');
+const current0El = document.querySelector('current--0');
+const current1El = document.querySelector('current--1');
