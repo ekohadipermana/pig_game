61359fe9d4738d7683bdf9b9620a6eaa915b47ff
ekohadipermana <ekohadipermana@gmail.com>
2024-02-18 08:08:04 +0700

    creating button elements for DOM

--- script2.js
+++ script2.js
@@ -3,3 +3,7 @@ const score0El = document.querySelector('score--0');
 const score1El = document.querySelector('score--1');
 const current0El = document.querySelector('current--0');
 const current1El = document.querySelector('current--1');
+const buttonNew = document.querySelector('btn--new');
+const buttonRoll = document.querySelector('btn--roll');
+const buttonHold = document.querySelector('btn--hold');
+
